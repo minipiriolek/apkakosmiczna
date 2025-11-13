ba44744303473eef99a178a7474925c0fab8ded6
olo <aleksander.malicki@cyfroweszkoly.pl>
2025-11-13 10:10:48 +0100

    first

---
+++ apkakosmiczna/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace apkakosmiczna.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ apkakosmiczna/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace apkakosmiczna.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
